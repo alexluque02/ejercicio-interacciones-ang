8709f705de21807b013f2da2b68ae177c65e6779
alexluque02 <alexanderluquehoffrogge@gmail.com>
2023-10-24 19:36:12 +0200

    Ejercicio hecho en casa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PeopleListComponent } from './components/people-list/people-list.component';
+import { PeopleItemComponent } from './components/people-item/people-item.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeopleListComponent,
+    PeopleItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
